450a213366b53839a129d8f157b4cfa49e45b241
Devin-Son <dtsddtsddtsd@gmail.com>
2024-12-08 12:39:24 -0800

    changed reader.py

--- reader.py
+++ reader.py
@@ -9,9 +9,10 @@ with open("openings.tsv") as file:
 
 import csv, numpy as np
 
-with open("openings.tsv") as file:
-    tsv_file = csv.reader(file, delimiter="\t")
-    #for line in tsv_file:
-        #print(line[1], " ", line[2])
+if __name__ == '__main__':
+    with open("openings.tsv") as file:
+        tsv_file = csv.reader(file, delimiter="\t")
+        for line in tsv_file:
+            print(line[1], " ", line[2])
 
 
